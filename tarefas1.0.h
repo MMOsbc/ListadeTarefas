5e5f7151ab4eb04d13835a0e811bc8902af7f897
Matheus Matos <115129142+MMOsbc@users.noreply.github.com>
2024-04-02 20:55:38 -0300

    Update tarefas1.0.h
    adicionando assinatura de funções

--- tarefas1.0.h
+++ tarefas1.0.h
@@ -1,1 +1,22 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+// type def do truck irá informar o que será permitido dentro dele e a quantidade de caracteres
+typedef struct {
+    int prioridade;
+    char categoria[100];
+    char descricao[300];
+} Tarefa;
+// define a quantidade maxima de tarefas
+typedef struct {
+    Tarefa tarefas[100];
+    int qtd;
+} ListaDeTarefas;
+// assinatura das funções
+void cadastrarTarefa(ListaDeTarefas *lt);
+void listarTarefas(ListaDeTarefas lt);
+void salvarTarefas(ListaDeTarefas lt, const char *arquivo);
+void carregarTarefas(ListaDeTarefas *lt, const char *arquivo);
+
+#endif
 
